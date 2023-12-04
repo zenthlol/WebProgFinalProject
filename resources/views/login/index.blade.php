30c3134383ce8cb13b416445e97d643803d4af95
YuricJulias <yuricjulias@gmail.com>
2023-12-05 00:11:04 +0700

    perbaiki login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,10 +1,14 @@
 @include('template.partials.head')
 
 <body>
-    <div class="row justifiy-content-center">
-        <div style="margin-left: 30%; margin-top:10%" class="col-md-4 border">
-            <main class="form-signin">
-                <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
+    <div class="row align-items-center justify-content-center h-100">
+        {{-- <div style="margin-left: 30%; margin-top:10%" class="col-md-4 border"> --}}
+        <div class=" col-md-4 border h-50 w-50 d-flex flex-column justify-content-center">
+            <main class="form-signin d-flex flex-column justify-content-center mx-auto gap-5 w-75">
+
+                <h1 class="h3 fw-normal text-center">Please login</h1>
+
+
                 {{-- Kalo user berhasil regist --}}
                 @if (session()->has('success'))
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -12,6 +16,8 @@
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 @endif
+
+
                 {{-- Kalo salah Login --}}
                 @if (session()->has('loginError'))
                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -19,17 +25,19 @@
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 @endif
-                <form action="/login" method="POST">
+
+
+                <form action="/login" method="POST" class="d-flex flex-column justify-content-center gap-3">
                     @csrf
                     <div class="form-floating">
                         <input type="email" name="email"
-                            class="form-control  
-                        @error('email')
-                          is-invalid
-                        @enderror"
+                            class="form-control border-success shadow-none
+                            @error('email')
+                              is-invalid
+                            @enderror"
                             id="floatingInput" placeholder="name@example.com" autofocus required
                             value="{{ old('email') }}">
-                            
+
                         <label for="floatingInput">Email address</label>
                         @error('email')
                             <div class="invalid-feedback">
@@ -38,12 +46,13 @@
                         @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" name="password" class="form-control" id="floatingPassword"
-                            placeholder="Password" required>
+                        <input type="password" name="password" class="form-control border-success shadow-none"
+                            id="floatingPassword" placeholder="Password" required>
                         <label for="floatingPassword">Password</label>
                     </div>
                     <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
                 </form>
+
                 <small class="d-block text-center">Not Registered? <a href="/register">Register Now!</a></small>
             </main>
         </div>
