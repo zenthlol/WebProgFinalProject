3dbdb17bf0dd1d1a9b923817056d58286e81b265
Kenneth <kenneth.tjiang@gmail.com>
2023-12-05 21:53:03 +0700

    benerin FE

--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -36,12 +36,12 @@
             <h1>================================================================</h1>
         @endforeach
     </div> --}}
-
+    <h1 class="m-3 d-flex justify-content-center ">Publishers</h1>
     <div class="accordion accordion-flush" id="accordionFlushExample">
         @foreach ($publishers as $publisher)
         <div class="accordion-item">
           <h2 class="accordion-header">
-            
+
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{ str_replace(' ', '', $publisher->name) }}">
                <h5>{{ $publisher->name }}</h5>
             </button>
@@ -50,14 +50,15 @@
 
           <div id="flush-collapse{{ str_replace(' ', '', $publisher->name) }}" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
             <div class="accordion-body">
+                <h5>Published Books:</h2>
                 <div class="d-flex flex-row">
                     @foreach ($publisher->publisherToBook as $item)
                         {{-- ini publisherToBook dari function di model publisher --}}
-                        <div class="card m-3 " style="width: 18rem;">
+                        <div class="card m-3 " style="width: 12rem;">
                             <img src="{{ asset('storage/' . $item->image) }}" class="card-img-top">
                             <div class="card-body">
-                                <h5 class="card-title">Book Title = {{ $item->title }}</h5>
-                                <p class="card-text">Written by</p>
+                                <h5 class="card-title">Book Title :<br> {{ $item->title }}<br></h5>
+                                <h5 class="card-text">Written by</h5>
                                 <h5 class="card-title">{{ $item->author }}</h5>
                             </div>
                         </div>
@@ -69,4 +70,6 @@
         </div>
         @endforeach
       </div>
+
+
 @endsection
--- resources/views/template/partials/navbar.blade.php
+++ resources/views/template/partials/navbar.blade.php
@@ -1,23 +1,20 @@
 <style>
-        .navbar.center .navbar-inner {
-        text-align: center;
+    /* Custom CSS */
+    .navbar {
+
+      padding: 10px 20px;
+      background-color: #f8f9fa;
+      box-shadow: 0 2px 4px rgba(0,0,0,0.1);
     }
 
-    .navbar.center .navbar-inner .nav {
-        display:inline-block;
-        float: none;
-    }
 </style>
 <div style="background-color: lightblue" class="d-flex justify-content-center">
     <h1>Inkwell and Quill BookStore</h1>
 </div>
-{{-- <div class="d-flex justify-content-center sticky-top"> --}}
-    <nav style="box-shadow: 0 2px 4px 0 rgba(0,0,0,.2); " class="navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-center sticky-top " >
-        <div class="container-fluid d-flex justify-content-cen  ter">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
+
+    <nav style="box-shadow: 0 2px 4px 0 rgba(0,0,0,.2); " class="navbar navbar-expand-lg bg-body-tertiary sticky-top d-flex" >
+        {{-- DIV 1 -=-=-------- --}}
+        <div class=" " id="navbarSupportedContent">
             <ul class="navbar-nav mb-2 mb-lg-0 d-flex justify-content-center">
               <li class="nav-item">
                 <a class="nav-link {{ ($active === "Home") ? 'active' : '' }}" aria-current="page" href="{{ route('welcome') }}">Home</a>
@@ -45,47 +42,55 @@
                     </li>
                 </ul>
               </li>
+            </ul>
+        </div>
 
+        {{-- DIV 2 ================= --}}
+        <div style="margin-left: 27%" class="" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 d-flex justify-content-center">
+              {{-- SEARCH--}}
             @if ($active === "Home")
-                {{-- SEARCH--}}
+
                 <form class="d-flex" action="/welcome">
                     <input class="form-control me-2" name ="search" type="search" placeholder="Search for Book Name" value="{{request('search')}}">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    <button class="btn btn-outline-primary" type="submit">Search</button>
                 </form>
             @endif
-
-
-            {{-- <ul class="navbar-nav ms-auto"> --}}
-              @auth
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Welcome back, {{auth()->user()->name}}
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="/profile"><i class="bi bi-person"></i> Profile</a></li>
-                    <li><a class="dropdown-item" href="/password"><i class="bi bi-lock"></i> Change Password</a></li>
-                    <li><a class="dropdown-item" href="/cart"><i class="bi bi-cart"></i> My Cart</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li>
-                        <form action="/logout" method="POST">
-                            @csrf
-                            <button type="submit" class="dropdown-item">
-                                <i class="bi bi-box-arrow-in-left"></i> Logout
-                            </button>
-                        </form>
-                    </li>
-                </ul>
-              </li>
-              @else
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-                </li>
-              @endauth
-            {{-- </ul> --}}
-
-
-            </div>
+            </ul>
         </div>
+
+        {{-- DIV 3 ================= --}}
+        <div style="margin-left: auto" class="" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 ">
+                {{-- PROFILE BAR --}}
+                    @auth
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Welcome back, {{auth()->user()->name}}
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/profile"><i class="bi bi-person"></i> Profile</a></li>
+                        <li><a class="dropdown-item" href="/password"><i class="bi bi-lock"></i> Change Password</a></li>
+                        <li><a class="dropdown-item" href="/cart"><i class="bi bi-cart"></i> My Cart</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <button type="submit" class="dropdown-item">
+                                    <i class="bi bi-box-arrow-in-left"></i> Logout
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                    </li>
+                    @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+                    </li>
+                    @endauth
+            </ul>
+        </div>
+
     </nav>
 
-{{-- </div> --}}
+
