60052f816841658571f5917ae54491edaf8d80db
YuricJulias <yuricjulias@gmail.com>
2023-12-06 09:36:19 +0700

    Update Footer + Publisher

--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -4,69 +4,55 @@
 @endsection
 
 @section('body')
-    {{-- <div class="m-5 d-flex-column justify-content-center">
-        @foreach ($publishers as $publisher) --}}
-            {{-- looping books jadi satu per satu dari banyak books di database --}}
-            {{-- <h1>Publisher:</h1>
-            <div class="card m-3" style="width: 18rem;">
-                <img src="{{ asset('storage/' . $publisher->image) }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Name : {{ $publisher->name }}</h5>
-                    <h5 class="card-title">Address : {{ $publisher->address }}</h5>
-                    <h5 class="card-title">Phone : {{ $publisher->phone }}</h5>
-                    <h5 class="card-title">Email : {{ $publisher->email }}</h5> --}}
+    {{-- Pakai Component Accordion Bootstrap --}}
+    <div class="container-fluid w-75 text-center">
+        <div class="accordion accordion-flush d-flex flex-column gap-4 py-3 " id="accordionFlushExample">
+            {{-- Judul --}}
+            <h1>Publisher List</h1>
 
-                    {{-- ini route() utk define bahwa routingan yg bernama bookById, kita panggil. dan akan passing variable id ke dalamnya --}}
-                {{-- </div>
-            </div>
-            <h1>Books Published:</h1>
-            <div class="d-flex flex-row">
-                @foreach ($publisher->publisherToBook as $item) --}}
-                    {{-- ini publisherToBook dari function di model publisher --}}
-                    {{-- <div class="card m-3 " style="width: 18rem;">
-                        <img src="{{ asset('storage/' . $item->image) }}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Book Title = {{ $item->title }}</h5>
-                            <p class="card-text">Written by</p>
-                            <h5 class="card-title">{{ $item->author }}</h5>
-                        </div>
-                    </div> --}}
-                {{-- @endforeach
-            </div>
-            <h1>================================================================</h1>
-        @endforeach
-    </div> --}}
+            {{-- List Publisher --}}
+            @foreach ($publishers as $publisher)
+                <div class="accordion-item border">
+                    <h2 class="accordion-header">
 
-    <div class="accordion accordion-flush" id="accordionFlushExample">
-        @foreach ($publishers as $publisher)
-        <div class="accordion-item">
-          <h2 class="accordion-header">
-            
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{ str_replace(' ', '', $publisher->name) }}">
-               <h5>{{ $publisher->name }}</h5>
-            </button>
-          </h2>
+                        {{-- Kotak per Publisher --}}
+                        <button
+                            class="
+                                accordion-button collapsed shadow-none btn btn-outline-primary"
+                            type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapse{{ preg_replace('/[^A-Za-z]/', '', $publisher->name) }}">
 
+                            {{-- Nama Publisher --}}
+                            <h5>{{ $publisher->name }}</h5>
 
-          <div id="flush-collapse{{ str_replace(' ', '', $publisher->name) }}" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-            <div class="accordion-body">
-                <div class="d-flex flex-row">
-                    @foreach ($publisher->publisherToBook as $item)
-                        {{-- ini publisherToBook dari function di model publisher --}}
-                        <div class="card m-3 " style="width: 18rem;">
-                            <img src="{{ asset('storage/' . $item->image) }}" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">Book Title = {{ $item->title }}</h5>
-                                <p class="card-text">Written by</p>
-                                <h5 class="card-title">{{ $item->author }}</h5>
+                            <i class="fa fa-angle-down"></i>
+                        </button>
+                    </h2>
+
+                    {{-- Buku apa saja yang dipublish oleh publisher --}}
+                    <div id="flush-collapse{{ preg_replace('/[^A-Za-z]/', '', $publisher->name) }}"
+                        class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            <div class="d-flex flex-row">
+
+                                {{-- Buku-buku --}}
+                                @foreach ($publisher->publisherToBook as $item)
+                                    <div class="card m-3 " style="width: 18rem;">
+                                        <img src="{{ asset('storage/' . $item->image) }}" class="card-img-top">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Title : {{ $item->title }}</h5>
+                                            <p class="card-text">Synopsis</p>
+                                            <h5 class="card-title">{{ $item->synopsis }}</h5>
+                                            <a href="{{ route('bookById', $item->id) }}" class="btn btn-primary">Details</a>
+                                        </div>
+                                    </div>
+                                @endforeach
+
                             </div>
                         </div>
-                    @endforeach
+                    </div>
                 </div>
-            </div>
-          </div>
-
+            @endforeach
         </div>
-        @endforeach
-      </div>
+    </div>
 @endsection
