498f7f834892ebc2b347a324af6f0b98596e8244
YuricJulias <yuricjulias@gmail.com>
2023-12-06 16:09:39 +0700

    Update AssignCategory

--- resources/views/bookCategory/assignCategory.blade.php
+++ resources/views/bookCategory/assignCategory.blade.php
@@ -4,35 +4,30 @@
 @endsection
 
 @section('body')
-<div class="container">
-    <div class="col-md-7 bg-light manage-wrapper">
-        <h3><i class="uil uil-plus"></i>Assign Book Categories</h1>
-        <p>--</p>
-        <hr>
-        <form action="{{ url('bookCategory/assignCategory') }}" method="POST">
-          @csrf
-
-
-            {{-- <div class="mb-3">
-              <label class="form-label">Book Title</label>
-              <input type="text" class="form-control @error('title') is-invalid @enderror" placeholder="Input book title" name="title" value="{{ old('title') }}">
-              @error('title') <small class="text-danger">{{ $message }}</small> @enderror
-            </div> --}}
-
-            <div class="mb-3">
-                <label class="form-label">Book</label>
-                <select class="form-select" aria-label="Default select example" name="book_id">
-                    <option selected>Select Book</option>
-                    @foreach ($books as $book)
-                        <option value="{{ $book->id }}">{{ $book->title }}</option>
-                    @endforeach
-                </select>
-                @error('book_id') <small class="text-danger">{{ $message }}</small> @enderror
+<div class="profile-height container-fluid d-flex justify-content-center align-items-center">
+    <div class="col-md-7 bg-light border border-dark rounded-5 align-items-center d-flex justify-content-center w-50 h-75">
+        <div class="w-75 d-flex flex-column gap-3">
+          <h3 class="text-center"><i class="bi bi-plus"></i>Assign Book Categories</h1>
+          <form action="{{ url('bookCategory/assignCategory') }}" method="POST">
+            @csrf
+              {{-- <div class="mb-3">
+                <label class="form-label">Book Title</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" placeholder="Input book title" name="title" value="{{ old('title') }}">
+                @error('title') <small class="text-danger">{{ $message }}</small> @enderror
+              </div> --}}
+              <div class="mb-3">
+                  <label class="form-label">Book</label>
+                  <select class="form-select shadow-none" aria-label="Default select example" name="book_id">
+                      <option selected>Select Book</option>
+                      @foreach ($books as $book)
+                          <option value="{{ $book->id }}">{{ $book->title }}</option>
+                      @endforeach
+                  </select>
+                  @error('book_id') <small class="text-danger">{{ $message }}</small> @enderror
               </div>
-
               <div class="mb-3">
                 <label class="form-label">Category Name</label>
-                <select class="form-select" aria-label="Default select example" name="category_id">
+                <select class="form-select shadow-none" aria-label="Default select example" name="category_id">
                     <option selected>Select Category</option>
                     @foreach ($categories as $category)
                         <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -40,24 +35,19 @@
                 </select>
                 @error('book_id') <small class="text-danger">{{ $message }}</small> @enderror
               </div>
-
-
-
-
-            {{-- <div class="mb-3">
-                <label class="form-label">Book ID</label>
-                <input type="integer" class="form-control @error('book_id') is-invalid @enderror" placeholder="Input book ID" name="book_id" value="{{ old('book_id') }}">
-                @error('book_id') <small class="text-danger">{{ $message }}</small> @enderror
-            </div>
-
-            <div class="mb-3">
-                <label class="form-label">Category ID</label>
-                <input type="integer" class="form-control @error('category_id') is-invalid @enderror" placeholder="Input category ID" name="category_id" value="{{ old('category_id') }}">
-                @error('category_id') <small class="text-danger">{{ $message }}</small> @enderror
-            </div> --}}
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+              {{-- <div class="mb-3">
+                  <label class="form-label">Book ID</label>
+                  <input type="integer" class="form-control @error('book_id') is-invalid @enderror" placeholder="Input book ID" name="book_id" value="{{ old('book_id') }}">
+                  @error('book_id') <small class="text-danger">{{ $message }}</small> @enderror
+              </div>
+              <div class="mb-3">
+                  <label class="form-label">Category ID</label>
+                  <input type="integer" class="form-control @error('category_id') is-invalid @enderror" placeholder="Input category ID" name="category_id" value="{{ old('category_id') }}">
+                  @error('category_id') <small class="text-danger">{{ $message }}</small> @enderror
+              </div> --}}
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
 
 
 
--- resources/views/template/partials/head.blade.php
+++ resources/views/template/partials/head.blade.php
@@ -4,15 +4,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- buat icon, nanti diganti sama logo --}}
     <link rel="icon" href="{{ asset('asset/favicon.png') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    {{-- bootstrap icon below--}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    {{-- bootstrap icon below --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-    <title>Inkwell and Quill BookStore | {{$title}}</title>
+    <title>Inkwell and Quill BookStore | {{ $title }}</title>
 
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700&display=swap');
-        
-        *{
+
+        * {
             font-family: 'Montserrat', sans-serif;
         }
 
