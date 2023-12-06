7731049c31e71fd05cdaef17555d0e271f184584
YuricJulias <yuricjulias@gmail.com>
2023-12-06 10:44:11 +0700

    Perbaiki Nav

--- resources/views/template/partials/navbar.blade.php
+++ resources/views/template/partials/navbar.blade.php
@@ -13,8 +13,12 @@
 </div>
 
     <nav style="box-shadow: 0 2px 4px 0 rgba(0,0,0,.2); " class="navbar navbar-expand-lg bg-body-tertiary sticky-top d-flex" >
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
         {{-- DIV 1 -=-=-------- --}}
-        <div class=" " id="navbarSupportedContent">
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
             <ul class="navbar-nav mb-2 mb-lg-0 d-flex justify-content-center">
               <li class="nav-item">
                 <a class="nav-link {{ ($active === "Home") ? 'active' : '' }}" aria-current="page" href="{{ route('welcome') }}">Home</a>
