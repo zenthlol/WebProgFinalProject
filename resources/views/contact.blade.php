d8fe2bf3dbabcb71163bfe62e86fc8550e0444c7
Kobel <nicholas.williem@binus.ac.id>
2023-12-10 23:13:28 +0700

    Tambahan contact us

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,7 +8,7 @@
     <section style='padding:50px; height: 100vh'>
         <div class="container">
             <div class="row">
-                <div class="col-md-6 offset-md-3">
+                <div class="col-md-6">
                     <div class="card">
                         <div class="card-header">
                             <h3 class="text-center">Contact Us</h3>
@@ -44,8 +44,79 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-md-6 p-5 mt-5 mb-5">
+                    <div class="col-md flex-wrap">
+                        <div class="container mt-5">
+                            <div class="row flex-wrap">
+                              <div class="col-md">
+                                <div class="d-flex align-items-center">
+                                    <!-- Icon -->
+                                    <div class="mr-2">
+                                        <h1>
+                                        <i class="bi bi-shop fs-1 me-3"></i>
+                                        </h1>
+                                    </div>
+
+                                    <!-- Text -->
+                                    <p class="mb-0 fs-5">
+                                        Ink And Quill BookStore
+                                        <br>
+                                        Jl. Menuju Magang no.1
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="container mt-5">
+                            <div class="row">
+                              <div class="col-md">
+                                <div class="d-flex align-items-center">
+                                    <!-- Icon -->
+                                    <div class="mr-2">
+                                        <h1>
+                                        <i class="bi bi-clock fs-1 me-3"></i>
+                                        </h1>
+                                    </div>
+
+                                    <!-- Text -->
+                                    <p class="mb-0 fs-5">
+                                        Open Every Day
+                                        <br>
+                                        from 09.00 - 17.00 WIB
+                                    </p>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="container mt-5">
+                            <div class="row">
+                              <div class="col-md">
+                                <div class="d-flex align-items-center">
+                                    <!-- Icon -->
+                                    <div class="mr-2">
+                                        <h1>
+                                        <i class="bi bi-whatsapp fs-1 me-3"></i>
+                                        </h1>
+                                    </div>
+
+                                    <!-- Text -->
+                                    <p class="mb-0 fs-5">
+                                        +62 812 3456 7890
+                                        <br>
+                                        Available from 09.00 - 17.00 WIB
+                                    </p>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
     </section>
 
 
